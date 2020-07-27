10a55793df6af5cceb9aa77c886e7cf02a8fa335
Mika <makomitsuki@gmail.com>
2020-07-27 12:09:07 -0500

    cron job innit

--- robotyulia.js
+++ robotyulia.js
@@ -2,16 +2,19 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
+var schedule = require('node-schedule');
 
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
+  var months
+  raidScheduler();
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content === '!are you here') {
+    msg.reply("yeah I'm here");
     msg.channel.send('ur mom');
 
   } else if (msg.content.startsWith('!kick')) {
@@ -23,3 +26,28 @@ bot.on('message', msg => {
     }
   }
 });
+
+function raidScheduler() {
+  // wednesday cron
+  var wedcron = new schedule.RecurrenceRule();
+  wedcron.dayOfWeek = 3;
+  wedcron.hour = 13;
+  wedcron.minute = 0;
+  var w = schedule.scheduleJob(wedcron, function(){
+    console.log('Wednesday');
+  });
+
+  // friday cron
+  var wedcron = new schedule.RecurrenceRule();
+  wedcron.dayOfWeek = 5;
+  wedcron.hour = 14;
+  wedcron.minute = 0;
+  var w = schedule.scheduleJob(wedcron, function(){
+    console.log('Friday');
+  });
+
+  // monday cron
+  var m = schedule.scheduleJob('0 13 * * 1 [ `expr \`date +\%s\` / 86400 \% 2` -eq 1 ] &&', function(){
+    console.log('Monday');
+  });
+}
